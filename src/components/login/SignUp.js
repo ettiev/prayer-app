fba3fa7277a2c7f7e28c1bbf743899278077952a
ettiev <126219891+ettiev@users.noreply.github.com>
2024-04-16 10:27:43 +0200

    Create Signup functionality
    -Added form
    - Added change handlers and a signup handler to send information to the backend

--- src/components/login/SignUp.js
+++ src/components/login/SignUp.js
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 import style from "./modalstyles.module.css";
 
 function SignUp() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    
+    function handleChangeUsername(event){
+        setUsername(event.target.value);
+    }
+
+    function handleChangeEmail(event){
+        setEmail(event.target.value);
+    }
+
+    function handleChangePassword(event){
+        setPassword(event.target.value);
+    }
+
+    function handleChangeConfirmPassword(event){
+        setConfirmPassword(event.target.value);
+    }
+
+    function submitHandler(event){
+        event.preventDefault();
+        const formData = {
+            username: username,
+            email: email,
+            password: password,
+            confirmPassword: confirmPassword
+        }
+        console.log(formData);
+
+        const url = "http://localhost:4000/new_user";
+        const options = {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+        };
+        fetch(url, options)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+
+    
     return <div className="modal-dialog modal-fullscreen" >
         <div id="signupModal" className="modal" tabIndex="-1">
             <div className="modal-dialog">
@@ -11,29 +57,31 @@ function SignUp() {
                         <h5 className="modal-title">Sign Up</h5>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div id={style.signup_body} className="modal-body">
-                        <p>Enter username, email and password to signup.</p>
-                        <div className="input-group mb-3">
-                            <span className="input-group-text" id="inputGroup-sizing-default" style={{ width: '161px' }}>Username:</span>
-                            <input type="name" className="form-control" />
+                    <form onSubmit={submitHandler}>
+                        <div id={style.signup_body} className="modal-body">
+                            <p>Enter username, email and password to signup.</p>
+                            <div className="input-group mb-3">
+                                <span className="input-group-text" id="inputGroup-sizing-default" style={{ width: '161px' }}>Username:</span>
+                                <input type="text" className="form-control" name="username" onChange={handleChangeUsername} value={username}/>
+                            </div>
+                            <div className="input-group mb-3">
+                                <span className="input-group-text" id="inputGroup-sizing-default" style={{ width: '161px' }}>Email:</span>
+                                <input type="email" className="form-control" name="email" onChange={handleChangeEmail} value={email}/>
+                            </div>
+                            <div className="input-group mb-3">
+                                <span className="input-group-text" id="inputGroup-sizing-default" style={{ width: '161px' }}>Password:</span>
+                                <input type="password" className="form-control" name="password" onChange={handleChangePassword} value={password}/>
+                            </div>
+                            <div className="input-group mb-3">
+                                <span className="input-group-text" id="inputGroup-sizing-default" style={{ width: '161px' }}>Confirm Password:</span>
+                                <input type="password" className="form-control" name="confirmPassword" onChange={handleChangeConfirmPassword} value={confirmPassword}/>
+                            </div>
                         </div>
-                        <div className="input-group mb-3">
-                            <span className="input-group-text" id="inputGroup-sizing-default" style={{ width: '161px' }}>Email:</span>
-                            <input type="email" className="form-control" />
+                        <div className="modal-footer">
+                            <button type="submit" className="btn btn-success">Sign Up</button>
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         </div>
-                        <div className="input-group mb-3">
-                            <span className="input-group-text" id="inputGroup-sizing-default" style={{ width: '161px' }}>Password:</span>
-                            <input type="password" className="form-control" />
-                        </div>
-                        <div className="input-group mb-3">
-                            <span className="input-group-text" id="inputGroup-sizing-default" style={{ width: '161px' }}>Confirm Password:</span>
-                            <input type="password" className="form-control" />
-                        </div>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-success">Sign Up</button>
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
